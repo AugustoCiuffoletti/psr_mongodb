398238ee350fb4063f59833ec7aae869b6a90f77
Augusto Ciuffoletti <augusto.ciuffoletti@gmail.com>
2020-12-14 17:47:56 +0100

    Aggiunto aggiornamento

---
+++ aggiorna.py
@@ -0,0 +1,14 @@
+import sys
+import dns
+import pymongo
+
+uri = 'mongodb+srv://<username>:<password>@examples.dfcdl.mongodb.net/<dbname>?retryWrites=true&w=majority'
+client = pymongo.MongoClient(uri)    # Connessione al server
+db = client.get_default_database()   # Accesso al DB
+collezione = db['archivio']          # Selezione della collezione
+collezione.update(
+   {'titolo': 'The Life and Opinions of Tristam Shandy'}, # selezione del documento
+   {'$set': {'autore': 'Laurence Sterne'}}                # modifica
+)
+client.close()                       # Chiusura della connessione
+
