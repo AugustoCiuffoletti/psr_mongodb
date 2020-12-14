c9f8fa03db7da640ab7bf5e0c63391e1aaa5228a
Augusto Ciuffoletti <augusto.ciuffoletti@gmail.com>
2020-12-14 17:22:49 +0100

    scorporato file libri

---
+++ libri.py
@@ -0,0 +1,18 @@
+libri = [
+    {  'scaffale': 'alto',
+       'autore': 'L. Sterne',
+       'titolo': 'The Life and Opinions of Tristam Shandy',
+       'prestito': False },
+    {  'scaffale': 'medio',
+       'autore': 'Edward Morgan Forster',
+       'titolo': 'Passage to India',
+       'prestito': True },
+    {  'scaffale': 'alto',
+       'autore': 'Charles Dickens',
+       'titolo': 'Great Expectations',
+       'prestito': False },
+    {  'scaffale': 'basso',
+       'autore': 'Charles Dickens',
+       'titolo': 'Hard Times',
+       'prestito': False }
+]
