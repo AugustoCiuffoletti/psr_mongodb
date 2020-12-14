5d482bf8db2582f1f2e42e5928ed7a1ffb17dd85
Augusto Ciuffoletti <augusto.ciuffoletti@gmail.com>
2020-12-14 17:27:05 +0100

    Post chiusura mlab

--- cerca.py
+++ cerca.py
@@ -1,7 +1,8 @@
 import sys
+import dns
 import pymongo
 
-uri = 'mongodb://user:password@ds111066.mlab.com:11066/example' 
+uri = 'mongodb+srv://<username>:<password>@examples.dfcdl.mongodb.net/<dbname>?retryWrites=true&w=majority'
 client = pymongo.MongoClient(uri)
 db = client.get_default_database()
 collezione = db['archivio']
